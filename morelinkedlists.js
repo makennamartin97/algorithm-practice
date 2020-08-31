c02155a7af2ada906c41c781a62f6c1e3145a354
makennamartin97 <makennamartin97@yahoo.com>
2020-08-30 23:51:12 -0400

    more list nodes

--- morelinkedlists.js
+++ morelinkedlists.js
@@ -54,32 +54,61 @@ class LinkedList {
         console.log("The last node is: " + lastnode.data);
         return lastnode;
     }
+    //get first node
     getFirst(){
         return this.head;
    
     }
+    //add new node to end 
+    add(data){
+        // creates a new node 
+        var newnode = new ListNode(data); 
+        // to store current node 
+        var current; 
+        //if list is empty add data/element and make it head
+        if(this.head == null){
+            this.head = newnode;
+        }else{
+            current = this.head;
+            //iterate thru end of the list
+            while(current.next){
+                current = current.next;
+            }
+            // add node
+            current.next = newnode;
+        }
+    }
 
 }
-//create 2 list nodes
+//create 4 list nodes
 let node1 = new ListNode(2)
 let node2 = new ListNode(5)
 let node3 = new ListNode(8)
 let node4 = new ListNode(11)
 
-//pointer to node2
+//pointer to node2, etc
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = null;
+
 //creating linked list with node1
 let list = new LinkedList(node1)
+
 //accessing a val
 //console.log(list.head.next.data)
+
 //getting size
 //list.size()
+
 //getting last node val
-console.log(list.getFirst())
+//console.log(list.getFirst())
+
 //clearing list
 //list.clear();
+
+//adding new node to end
+list.add(15)
+
 //printing list
 list.print();
