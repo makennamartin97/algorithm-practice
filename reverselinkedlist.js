a496fa41285391a27153c282104cf35f82af6fe4
makennamartin97 <makennamartin97@yahoo.com>
2020-09-10 19:21:20 -0400

    another reverse linked list solution added

--- reverselinkedlist.js
+++ reverselinkedlist.js
@@ -23,4 +23,21 @@ function reverselinkedlist(headoflist){
     return previousNode;
 
 }
-console.log(reverselinkedlist(headoflist));
+//console.log(reverselinkedlist(headoflist));
+
+//another solution
+
+var reverseList = function(head) {
+    let next = head;
+    let prev = null;
+    while(head){
+        next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+        
+        
+    }
+    return prev;
+    
+};
