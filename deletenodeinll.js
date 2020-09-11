1ddb00e5ec7d32fea5a15654cbbeced7686ceca3
makennamartin97 <makennamartin97@yahoo.com>
2020-09-10 23:44:09 -0400

    delete specific node from linked list w no head given

---
+++ deletenodeinll.js
@@ -0,0 +1,19 @@
+//given a node to delete, not head, delete the node in the linked list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+
+    
+};
