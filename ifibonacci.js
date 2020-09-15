4d19b4e68e3f8c44fad082296488818655d555cf
makennamartin97 <makennamartin97@yahoo.com>
2020-09-15 18:39:22 -0400

    adding to fibonacci file

--- ifibonacci.js
+++ ifibonacci.js
@@ -1,11 +1,17 @@
+
+//recursion
+//runtime complexity - exponential - O(2^n)
+//memory complexity = linear - O(n)
 function fiboRec(n){
     if(n <= 1){
         return n;
-    }else{
-        return fiboRec(n-2) + fiboRec(n-1);
     }
+    return fiboRec(n-2) + fiboRec(n-1);
+    
 }
-//console.log(fiboRec(2));
+//iteration
+//runtime complexity - linear - O(n)O(n).
+//memory complexity - constant - O(1)O(1).
 function fiboIter(n){
     var fibs = [0,1];
     for(var i = 0; i < n; i++){
