0748cabc1052d8c9c924e7dbd6b5d00acf70dce4
makennamartin97 <makennamartin97@yahoo.com>
2020-10-26 22:21:36 -0400

    reverse string in python

--- strings/reversestr.py
+++ strings/reversestr.py
@@ -1,5 +1,6 @@
 # reverse a string in python
 
+# backwards iteration pushing into new string
 def reverseString(str):
     result = ""
     for s in reversed(range(len(str))):
@@ -11,3 +12,11 @@ def reverseString(str):
 # testing
 print(reverseString("hello")) # olleh
 print(reverseString("palm trees")) # seert mlap
+
+# slice statement starting at the end of the string and going backwards by 1 (-1)
+def reverseString2(str):
+    str = str[::-1]
+    return str
+
+print(reverseString2("hello")) # olleh
+print(reverseString2("palm trees")) # seert mlap
