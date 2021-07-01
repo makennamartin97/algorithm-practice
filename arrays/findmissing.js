1f704b6b965f274ed0c719f6176a06a130f8ae4b
makennamartin97 <makennamartin97@yahoo.com>
2021-07-01 17:38:19 -0400

    find missing value in array

---
+++ arrays/findmissing.js
@@ -0,0 +1,18 @@
+//find the missing number in an array
+//you are given an array of positive numbers from 1 to n, all which are present except for 'x'
+//input array is not sorted
+function findmissing(arr){
+    // find sum of nums in list, linear scan O(n)
+    let sum = 0;
+    for(let i in arr){
+        sum += arr[i];
+    }
+    //since the arr given is 1 to n, let's declare n
+    //1 is missing in arr
+    let n = arr.length+1;
+    let sumwithn = Math.floor((n*(n+1))/2)
+    return sumwithn - sum;
+
+}
+const arr = [4,1,3,6,2];
+console.log(findmissing(arr));
